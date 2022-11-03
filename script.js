7c6658c76965258ff01a4b7c5f91584d833a7680
Paris Isaac <parisisaac65@gmail.com>
2022-11-03 09:38:50 -0400

    Fix error message for password

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+
+let password1 = document.getElementById("password");
+let password2 = document.getElementById("confirm_password"); 
+let pwError = document.getElementById("password-error")
+
+password2.addEventListener("keyup",verifyPasswordsMatch);
+
 function verifyPasswordsMatch(){
-    let password1 = document.getElementById("password").value;
-    let password2 = document.getElementById("confirm_password").value; 
     let match = true;
-    if (password1 != password2){
-        document.getElementById("password").classList.toggle("error");
-        document.getElementById("confirm_password").classList.toggle("error"); 
+    if (password1.value != password2.value){
+        password1.classList.toggle("error");
+        password2.classList.toggle("error");
+        pwError.textContent = "Passwords do not match";
         match = false;
     }
     return match; 
 }
 
-verifyPasswordsMatch(); 
 
